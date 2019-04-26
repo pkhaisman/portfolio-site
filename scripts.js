db7e3a288ae8727c95495dd97f0a17925c6f0a53
Philip Khaisman <pkhaisman@gmail.com>
2019-04-26 12:27:42 -0400

    finish nav link feature. pretty much done

--- scripts.js
+++ scripts.js
@@ -5,21 +5,23 @@ hamburgerIcon.addEventListener('click', () => {
     navItems.classList.toggle('display-nav-links');
 })
 
-function addScroll(node, scrollTo) {
-    node.addEventListener('click', () => {
-        window.scroll({
-            top: scrollTo,
-            behavior: 'smooth'
-        });
-    })
-}
+// function addScroll(node, scrollTo) {
+//     node.addEventListener('click', () => {
+//         window.scroll({
+//             top: scrollTo,
+//             behavior: 'smooth'
+//         });
+//     })
+// }
 
-let aboutLink = document.querySelector('.about');
-let projectsLink = document.querySelector('.project');
-let contactLink = document.querySelector('.contact');
-addScroll(aboutLink, 700);
-addScroll(projectsLink, 1395.5);
-addScroll(contactLink, 2400);
+// let aboutLink = document.querySelector('.about');
+// let aboutPosition = document.querySelector('.about-section').offsetTop;
+// let projectsLink = document.querySelector('.project');
+// let projectSection = document.querySelector('.project-section').offsetTop
+// let contactLink = document.querySelector('.contact');
+// addScroll(aboutLink, aboutPosition);
+// addScroll(projectsLink, projectSection);
+// addScroll(contactLink, 2400);
 
 
 
