300d907d434e32f3faea72d0d3c4a2cde05387c6
Philip Khaisman <pkhaisman@gmail.com>
2019-04-26 11:26:39 -0400

    complete auto scroll on nav links for big screens

--- scripts.js
+++ scripts.js
@@ -2,6 +2,25 @@ let hamburgerIcon = document.querySelector('.nav__hamburger-icon');
 let navItems = document.querySelector('.nav__list');
 
 hamburgerIcon.addEventListener('click', () => {
-    console.log('click');
     navItems.classList.toggle('display-nav-links');
 })
+
+function addScroll(node, scrollTo) {
+    node.addEventListener('click', () => {
+        window.scroll({
+            top: scrollTo,
+            behavior: 'smooth'
+        });
+    })
+}
+
+let aboutLink = document.querySelector('.about');
+let projectsLink = document.querySelector('.project');
+let contactLink = document.querySelector('.contact');
+addScroll(aboutLink, 700);
+addScroll(projectsLink, 1395.5);
+addScroll(contactLink, 2400);
+
+
+
+
