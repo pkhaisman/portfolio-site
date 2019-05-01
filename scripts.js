50466179d95d6070a389da9c90ad787584dcf8c3
Philip Khaisman <pkhaisman@gmail.com>
2019-05-01 13:41:23 -0400

    optimize for large screen. increase size of contact section

--- scripts.js
+++ scripts.js
@@ -5,23 +5,7 @@ hamburgerIcon.addEventListener('click', () => {
     navItems.classList.toggle('display-nav-links');
 })
 
-// function addScroll(node, scrollTo) {
-//     node.addEventListener('click', () => {
-//         window.scroll({
-//             top: scrollTo,
-//             behavior: 'smooth'
-//         });
-//     })
-// }
 
-// let aboutLink = document.querySelector('.about');
-// let aboutPosition = document.querySelector('.about-section').offsetTop;
-// let projectsLink = document.querySelector('.project');
-// let projectSection = document.querySelector('.project-section').offsetTop
-// let contactLink = document.querySelector('.contact');
-// addScroll(aboutLink, aboutPosition);
-// addScroll(projectsLink, projectSection);
-// addScroll(contactLink, 2400);
 
 
 
